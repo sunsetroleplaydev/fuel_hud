6335f580a40d023be355ecb733b25868865e8b33
Marshular's Development <78507312+marshular@users.noreply.github.com>
2023-04-16 11:23:35 -0700

    Delete config_server.lua

--- config_server.lua
+++
@@ -1,6 +0,0 @@
--- For support join my discord: https://discord.gg/Z9Mxu72zZ6
-
-server_config = {
-    discordServerToken = "", -- discord bot token for permissions
-    guildId = "", -- discord guild id
-}
