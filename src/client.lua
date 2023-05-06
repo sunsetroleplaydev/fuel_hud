6db5a2900de5e6e5f5ed94b564da5273c70e72dc
Marshular's Development <78507312+marshular@users.noreply.github.com>
2023-05-06 15:35:46 -0700

    Update client.lua

--- src/client.lua
+++ src/client.lua
@@ -18,7 +18,9 @@ CreateThread(function()
     while true do
         Wait(0)
         if vehicle ~= 0 and vehClass ~= 13 and driver then
-            local fuelLevel = (0.141 * GetVehicleFuelLevel(vehicle)) / 100 -- Fuel Value x Max Bar Width Show The Level Range Within The Bar
+            if config.fuelSystem == "nd" then
+                local fuelLevel = (0.141 * GetVehicleFuelLevel(vehicle)) / 100 -- Fuel Value x Max Bar Width Show The Level Range Within The Bar
+            end       
             DrawRect(0.0855, 0.8, 0.141, 0.010 + 0.006, 40, 40, 40, 150)  -- Bar Background (Black)
             if config.electricVehiles[GetEntityModel(vehicle)] then
                 DrawRect(0.0855, 0.8, 0.141, 0.010, 20, 140, 255, 100)  -- Bar Background (lighter blue)
